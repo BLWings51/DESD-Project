896161a769c9adc76767f6e50e15837ebbd9de1e
DevilExposed <plamen2.tyufekchiev@live.uwe.ac.uk>
2025-03-25 16:45:51 +0000

    Tweaks
    Tweaks

--- UniHub/main/urls.py
+++ UniHub/main/urls.py
@@ -1,6 +1,6 @@
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.urls import path
-from main import signup, views, profile
+from main import signup, views, ProfileSettings
 
 urlpatterns = [
     path('login/', views.CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
@@ -8,7 +8,7 @@ urlpatterns = [
     path('logout/', views.logout),
     path('authenticated/', views.is_authenticated),
     path('signup/', signup.SignupView),
-    path('ProfileSettings/', profile.UpdateProfileView)
-    path('admin_check/', is_admin),
+    path('ProfileSettings/', ProfileSettings.UpdateProfileView),
+    path('admin_check/', views.is_admin),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
