6d3f169d7068d5553a1e1bfcd02a4b10e421c6e3
mt5-islam <131805599+SORBX@users.noreply.github.com>
2025-03-27 16:42:58 +0000

    .

--- UniHub/main/serializer.py
+++ UniHub/main/serializer.py
@@ -4,29 +4,27 @@ from .models import Account, Society
 class SignupSerializer(serializers.ModelSerializer):
     class Meta:
         model=Account
-        fields = ['email', 'password']
+        fields = ['email', 'password', 'firstName', 'lastName']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        account = Account(email=validated_data['email'])
+        account = Account(email=validated_data['email'], firstName=validated_data['firstName'], lastName=validated_data['lastName'])
         account.set_password(validated_data['password'])
         account.save()
         return account
     
-# class SocietySerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model=SocietyRelation
-#         fields = ['society', 'account', 'adminStatus']
-#         extra_kwargs = {'adminStatus': {'write_only': False}}
-
-#     def create(self, validated_data):
-#         society = SocietyRelation(society=validated_data['society'])
-#         account = Account(email=validated_data['email'])
-#         account._state(validated_data['adminStatus'])
-#         account.save()
-#         return account
 
 class SocietySerializer(serializers.ModelSerializer):
     class Meta:
         model = Society
-        fields = '__all__'  # This includes all fields
+        fields = ['name', 'numOfInterestedPeople', 'description']
+
+    def create(self, validated_data):
+        society = Society.objects.create(**validated_data)
+        society.save()
+        return society
+    
+    def validate_name(self, value):
+        if Society.objects.filter(name=value).exists():
+            raise serializers.ValidationError("Society with this name already exists.")
+        return value
