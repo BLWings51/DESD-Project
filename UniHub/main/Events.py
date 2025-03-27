c1e76d56f42f3dfef935f3324d9deb64def0a9db
S1edgehammer5000 <124877607+S1edgehammer500@users.noreply.github.com>
2025-03-27 17:19:33 +0000

    Events Creation is finished

--- UniHub/main/Events.py
+++ UniHub/main/Events.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.generics import get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+from datetime import timedelta
 
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
@@ -15,7 +16,11 @@ from .models import Event, Society
 @permission_classes([IsAuthenticated])
 def CreateEvent(request, society_name):
     society = get_object_or_404(Society, name=society_name)
-    serializer = CreateEventSerializer(data=request.data)
+    serializer = CreateEventSerializer(data=request.data, context={'society': society})
+    startTime = request.data.get('startTime')
+    endTime = request.data.get('endTime')
+    if endTime <= startTime:
+        return Response({"error": "End date cannot be equal to or before the start date"}, status=400)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data)
@@ -24,10 +29,11 @@ def CreateEvent(request, society_name):
 class CreateEventSerializer(serializers.ModelSerializer):
     class Meta:
         model=Event
-        fields = ['society', 'name', 'details', 'startTime', 'endTime', 'location']
+        fields = ['name', 'details', 'startTime', 'endTime', 'location']
 
 
     def create(self, validated_data):
-        event = Event(society=self.context.get('society_name'), name=validated_data['name'], details=validated_data['details'], startTime=validated_data['startTime'], endTime=validated_data['endTime'], location=validated_data['location'])
+        society = self.context.get('society')
+        event = Event(society=society, name=validated_data['name'], details=validated_data['details'], startTime=validated_data['startTime'], endTime=validated_data['endTime'], location=validated_data['location'])
         event.save()
         return event
