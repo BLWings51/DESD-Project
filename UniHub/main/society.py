df492073c608abd12148322c880116dc2a6a9bb9
mt5-islam <131805599+SORBX@users.noreply.github.com>
2025-03-27 16:06:25 +0000

    Society

---
+++ UniHub/main/society.py
@@ -0,0 +1,28 @@
+from rest_framework import generics
+from .models import Society
+from .serializer import SocietySerializer, serializers
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def society_create(request):
+    serializer = SocietySerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+# List all societies or create a new one
+class SocietyListCreateView(generics.ListCreateAPIView):
+    queryset = Society.objects.all()
+    serializer_class = SocietySerializer
+    #permission_classes = [IsAuthenticated]  # Requires authentication
+
+# Retrieve a specific society
+class SocietyDetailView(generics.RetrieveAPIView):
+    queryset = Society.objects.all()
+    serializer_class = SocietySerializer
+    #permission_classes = [IsAuthenticated]  # Requires authentication
